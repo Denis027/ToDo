6de2692a163858d7d6c57b6c74b644937d58f29a
DenisNB <gmailspb@mail.ru>
2024-09-20 21:52:25 +0300

    init

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,13 @@
-//Находим элементы на странице
 const addToDoForm = document.querySelector("#addToDoForm");
 const addBtn = document.querySelector("#submitBtn");
 const taskInput = document.querySelector("#taskInput");
 const tasksList = document.querySelector("#tasksList");
 const emptyList = document.querySelector("#emptyList");
 
-//Добавляем задачу
+let tasks = [];
+
 addToDoForm.addEventListener("submit", addTask);
-
-//Удаляем задачу
 tasksList.addEventListener("click", deleteTask);
-
-//Задача выполнена
 tasksList.addEventListener("click", doneTask);
 
 //Функции
@@ -22,9 +18,13 @@ function addTask(event) {
   //Берем значиние из input
   const taskText = taskInput.value;
 
+  const task = { id: Date.now(), title: taskInput.value, done: false };
+
+  const clsCSS = task.done ? "tasksListItem done" : "tasksListItem";
+
   //Создаем разметку новой задачи
-  const taskHTML = `<li class="tasksListItem" id="listItem">
-                     <span>${taskText}</span>
+  const taskHTML = `<li id="${task.id}" class="${clsCSS}">
+                     <span>${task.title}</span>
                      <div id="taskBtnWrap">
                       <button data-action="done" id="taskBtnСhange">Готово</button>
                       <button data-action="delete" id="taskBtnDelete">Удалить</button>
@@ -32,6 +32,7 @@ function addTask(event) {
                    </li>`;
 
   //Добавляем новую задачу
+  tasks.push(task);
   tasksList.insertAdjacentHTML("beforeend", taskHTML);
 
   //Очищаем input и переводим фокус на него
@@ -45,10 +46,15 @@ function addTask(event) {
 }
 
 function deleteTask(event) {
-  if (event.target.dataset.action === "delete") {
-    const parentNode = event.target.closest(".tasksListItem");
-    parentNode.remove();
-  }
+  if (event.target.dataset.action !== "delete") return;
+
+  const parentNode = event.target.closest(".tasksListItem");
+  parentNode.remove();
+
+  const id = Number(parentNode.id);
+
+  tasks = tasks.filter((task) => task.id !== id);
+  console.log(tasks);
 
   if (tasksList.children.length === 1) {
     emptyList.classList.remove("none");
@@ -56,8 +62,17 @@ function deleteTask(event) {
 }
 
 function doneTask(event) {
-  if (event.target.dataset.action === "done") {
-    const parentNode = event.target.closest(".tasksListItem");
-    parentNode.classList.add("done");
-  }
+  if (event.target.dataset.action !== "done") return;
+
+  const parentNode = event.target.closest(".tasksListItem");
+  parentNode.classList.toggle("done");
+
+  const id = Number(parentNode.id);
+  tasks.find((task) => {
+    if (task.id === id) {
+      task.done = !task.done;
+    }
+  });
+
+  console.log(tasks);
 }
